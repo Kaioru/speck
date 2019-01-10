71b9ed7a56162675ecb6e0c2dec035c08e7c7fc2
Kaioru <keifuurawrs@gmail.com>
2019-01-10 19:36:30 +0800

    Add temporary logic for various spell corrections

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,44 @@ int main() {
     auto tree = Trie();
 
     tree.insert("hello");
-    tree.insert("hello");
-    tree.insert("my");
-    tree.insert("name");
-    tree.insert("is");
-    tree.insert("dibo");
 
-    for (auto &i : *tree.all())
+    auto words = *tree.all();
+
+    std::string word = "hello";
+    auto splits = std::vector<std::pair<std::string, std::string>>();
+    auto deletes = std::vector<std::string>();
+    auto transposes = std::vector<std::string>();
+    auto replaces = std::vector<std::string>();
+    auto inserts = std::vector<std::string>();
+
+    for (int i = 0; i < word.length() + 1; i++) {
+        splits.push_back(*new std::pair<std::string, std::string>(
+                word.substr(0, i),
+                word.substr(i)
+        ));
+    }
+
+    for (auto &i : splits) {
+        if (i.second.length() > 0) {
+            deletes.push_back(i.first + i.second.substr(1));
+
+            if (i.second.length() > 1) {
+                transposes.push_back(
+                        i.first + i.second[1] + i.second[0] + i.second.substr(2)
+                );
+            }
+        }
+
+        for (int c = 0; c < DOMAIN_SIZE; c++) {
+            if (i.second.length() > 0)
+                replaces.push_back(i.first + static_cast<char>(c + 'a') + i.second.substr(1));
+            inserts.push_back(i.first + static_cast<char>(c + 'a') + i.second);
+        }
+    }
+
+
+    for (auto &i : inserts) {
         std::cout << i << std::endl;
+    }
     return 0;
 }
