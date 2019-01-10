d118b87b838cd3cf6fc3ff51a7d58821ec8b8f04
Kaioru <keifuurawrs@gmail.com>
2019-01-10 22:08:02 +0800

    Add temporary cli thingy

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include "trie.h"
 #include "speck.h"
 #include "filet.h"
@@ -14,6 +15,27 @@ int main() {
 
     for (auto &row: filet) tree.insert(row[0]);
 
-    std::cout << speck.correct("henlo") << std::endl;
+    // TODO: a nice cli menu
+    auto suggestions = speck.suggestions("henlo");
+
+    std::cout << "Did you mean: " << suggestions[0] << "?";
+    if (suggestions.size() > 1) {
+        std::vector<std::string> others(suggestions.begin() + 1,
+                                        suggestions.begin() + (suggestions.size() > 4
+                                                               ? 4
+                                                               : 4 - suggestions.size()));
+
+        auto ss = std::stringstream();
+
+        ss << " Or maybe: ";
+        for (int i = 0; i < others.size(); i++) {
+            ss << others[i];
+            if (i < others.size() - 2) ss << ", ";
+            else if (i == others.size() - 2) ss << " or ";
+            else ss << "?";
+        }
+
+        std::cout << ss.str();
+    }
     return 0;
 }
